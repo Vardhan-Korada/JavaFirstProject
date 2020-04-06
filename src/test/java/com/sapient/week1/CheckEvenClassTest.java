d73a98dd92e7a50a263e7390b49d0c99371f1af4
vardhankorada <srivardhankorada@gmail.com>
2020-04-06 13:30:02 +0530

    Added gitignore

--- src/test/java/com/sapient/week1/CheckEvenClassTest.java
+++ src/test/java/com/sapient/week1/CheckEvenClassTest.java
@@ -1,9 +1,8 @@
 package com.sapient.week1;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 
 public class CheckEvenClassTest {
